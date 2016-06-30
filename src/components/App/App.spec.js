be68d99e78303917897282fb29832e7f4054a1ef
kionazaki <kio.nazaki@gmail.com>
2016-06-30 20:10:15 +0300

    1.0.10
    Jasmine: React components

--- src/components/App/App.spec.js
+++ src/components/App/App.spec.js
@@ -1,53 +1,12 @@
 import TestUtils from "react-addons-test-utils";
-//import ExpandedTestUtils from "expanded-react-test-utils";
 import TestUtilsAdditions from "react-testutils-additions";
-
-import React    from "react";
-//import ReactDOM from "react-dom";
+import React from "react";
 import App from "app/components/App/App";
-//import state$ from "app/state";
-import currentState from "app/rx-state/currentState";
-
-
-
-
-import Rx from "rxjs";
-import reducer$ from "app/rx-state/reducer";
-import automation from "app/rx-state/automation";
-import createState from "app/rx-state/createState";
-import templateState from "app/rx-state/templateState";
-
-import TodoClearCompletedFunc from "app/components/TodoClearCompleted/TodoClearCompletedFunc";
-
-
-
-
-const templateState2 = {
-    todos: {
-        toggleAll: false,
-        newTodoValue: '',
-        items: [
-            {label: 'test', checked: false, restoreLabel: ''},
-            {label: 'test2', checked: true, restoreLabel: ''}
-        ]
-    }
-};
-
-
-
-
-// Создание потока состояния
-const initialState$ = Rx.Observable.of(templateState);
-var state$ = automation(createState(reducer$, initialState$));
-
-
-// Обнуляем STATE по шаблону
-localStorage.setItem('state', JSON.stringify(currentState));
 
 // Рендерим весь App с самого корня
 const tree = TestUtils.renderIntoDocument(<App />);
 
-describe("App", () => {
+describe("React components", () => {
 
     it("Список пуст, компонент TodoHeader должен существовать", () => {
         const elems = TestUtils.scryRenderedDOMComponentsWithClass(tree, "new-todo");
@@ -150,7 +109,115 @@ describe("App", () => {
         expect(node_li.className).toEqual('');
     });
 
+    it("TodoHeader: добавление еще одной записи в список", () => {
+        const newTodo = TestUtilsAdditions.find(tree, ".new-todo")[0];
+        TestUtils.Simulate.keyDown(newTodo, {key: "Enter", target: {value: "Hallo world"} });
+        const elems = TestUtilsAdditions.find(tree, ".view");
+        expect(elems.length).toEqual(2);
+    });
 
+    it("TodoCount: В счетчике прописано '2 items left'", () => {
+        const nodeСount = TestUtilsAdditions.find(tree, ".todo-count")[0];
+        expect(nodeСount.textContent).toEqual('2 items left');
+    });
+
+    it("Отметить первую запись (checked) и убедиться, что в счетчике прописано '1 items left'", () => {
+        const nodeСheckbox = TestUtilsAdditions.find(tree, ".todo-list  .toggle")[0];
+        TestUtils.Simulate.change(nodeСheckbox);
+        const node_count = TestUtilsAdditions.find(tree, ".todo-count")[0];
+        expect(node_count.textContent).toEqual('1 items left');
+    });
+
+    it("TodoToggleAll:  ToggleAll.checked = false", () => {
+        const nodeToggleAll = TestUtilsAdditions.find(tree, ".toggle-all")[0];
+        expect(nodeToggleAll.checked).toBeFalsy();
+    });
+
+    it("Отметить вторую запись (checked) и убедиться что ToggleAll.checked = true", () => {
+        const nodeСheckbox = TestUtilsAdditions.find(tree, ".todo-list  .toggle")[1];
+        TestUtils.Simulate.change(nodeСheckbox);
+        const nodeToggleAll = TestUtilsAdditions.find(tree, ".toggle-all")[0];
+        expect(nodeToggleAll.checked).toBeTruthy();
+    });
+
+    it("Снять отметку с первой записи (checked) и убедиться, что ToggleAll.checked = false", () => {
+        const nodeСheckbox = TestUtilsAdditions.find(tree, ".todo-list  .toggle")[0];
+        TestUtils.Simulate.change(nodeСheckbox);
+        const nodeToggleAll = TestUtilsAdditions.find(tree, ".toggle-all")[0];
+        expect(nodeToggleAll.checked).toBeFalsy();
+    });
+
+    it("Сделать ToggleAll.checked = true и убедиться, что оба элемента списка стали выбранными", () => {
+        const nodeToggleAll = TestUtilsAdditions.find(tree, ".toggle-all")[0];
+        TestUtils.Simulate.change(nodeToggleAll);
+        const nodeСheckboxs = TestUtilsAdditions.find(tree, ".todo-list  .toggle");
+        let result = true;
+        nodeСheckboxs.forEach((item)=>{
+            result = result && item.checked;
+        });
+        expect(result).toBeTruthy();
+    });
+
+    it("Сделать ToggleAll.checked = false и убедиться, что оба элемента списка стали не выбранными", () => {
+        const nodeToggleAll = TestUtilsAdditions.find(tree, ".toggle-all")[0];
+        TestUtils.Simulate.change(nodeToggleAll);
+        const nodeСheckboxs = TestUtilsAdditions.find(tree, ".todo-list  .toggle");
+        let result = false;
+        nodeСheckboxs.forEach((item)=>{
+            result = result || item.checked;
+        });
+        expect(result).toBeFalsy();
+    });
+
+    it("TodoItem: отредактировать первую запись в пустое значение и убедиться, что она пропадает из списка", () => {
+        const nodeLi = TestUtilsAdditions.find(tree, ".todo-list li")[0];
+        TestUtils.Simulate.doubleClick(nodeLi);
+        const nodeEdit = TestUtilsAdditions.find(tree, ".edit")[0];
+        nodeEdit.value = '';
+        TestUtils.Simulate.change(nodeEdit);
+        TestUtils.Simulate.keyDown(nodeEdit, {key: "Enter" });
+        const elems = TestUtils.scryRenderedDOMComponentsWithClass(tree, "view");
+        expect(elems.length).toEqual(1);
+    });
+
+    it("TodoItem: нажать на крестик у оставшейся записи и убедиться, что она пропадает из списка", () => {
+        const nodeDestroy = TestUtilsAdditions.find(tree, ".destroy")[0];
+        TestUtils.Simulate.click(nodeDestroy);
+        const elems = TestUtils.scryRenderedDOMComponentsWithClass(tree, "view");
+        expect(elems.length).toEqual(0);
+    });
+
+    it("Список пуст, компонент TodoMain не должен существовать", () => {
+        const elems = TestUtils.scryRenderedDOMComponentsWithClass(tree, "main");
+        expect(elems.length).toEqual(0);
+    });
+
+    it("Список пуст, компонент TodoFooter не должен существовать", () => {
+        const elems = TestUtils.scryRenderedDOMComponentsWithClass(tree, "footer");
+        expect(elems.length).toEqual(0);
+    });
+
+    it("TodoClearCompleted: добавить два новых элемента в список, убедиться, что кнопка 'clear Comleted' не активна", () => {
+        const newTodo = TestUtils.findRenderedDOMComponentWithClass(tree, "new-todo");
+        TestUtils.Simulate.keyDown(newTodo, {key: "Enter", target: {value: "test value 1"} });
+        TestUtils.Simulate.keyDown(newTodo, {key: "Enter", target: {value: "test value 2"} });
+        const nodeСlearСompleted = TestUtilsAdditions.find(tree, ".clear-completed");
+        expect(nodeСlearСompleted.length).toEqual(0);
+    });
+
+    it("TodoClearCompleted: Выбрать первый элемнт (checked), кнопка 'clear Comleted' становится активной", () => {
+        const nodeСheckbox = TestUtilsAdditions.find(tree, ".todo-list  .toggle")[0];
+        TestUtils.Simulate.change(nodeСheckbox);
+        const nodeСlearСompleted = TestUtilsAdditions.find(tree, ".clear-completed");
+        expect(nodeСlearСompleted.length).toEqual(1);
+    });
+
+    it("TodoClearCompleted: При нажатии на 'clear Comleted' перва запись пропадает, вторая остаётся", () => {
+        const nodeСlearСompleted = TestUtilsAdditions.find(tree, ".clear-completed")[0];
+        TestUtils.Simulate.click(nodeСlearСompleted);
+        const nodeTodoList = TestUtilsAdditions.find(tree, ".todo-list")[0];
+        expect(nodeTodoList.textContent).toEqual("test value 2");
+    });
 
 });
 
